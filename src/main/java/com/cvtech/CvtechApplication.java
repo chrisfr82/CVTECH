dfb18741e50f707813f33b98e8298ff854bb7219
chris <clohez@efficom-lille.com>
2021-03-03 11:24:01 +0100

    firstCommit_CL

---
+++ src/main/java/com/cvtech/CvtechApplication.java
@@ -0,0 +1,13 @@
+package com.cvtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvtechApplication.class, args);
+	}
+
+}
